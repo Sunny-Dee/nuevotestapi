6dd9e6c54930abfbce659a6f8397b59ca5a634af
deescoba <deescoba@microsoft.com>
2020-01-06 23:10:06 -0800

    Add project files.

---
+++ Models/ImpactStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NuevoFoundationApiVersion2.Models
+{
+    public class ImpactStats
+    {
+        public string Title { get; set; }
+        public string TitleBoldened { get; set; }
+        public string Stat { get; set; }
+        public string Description { get; set; }
+    }
+}
